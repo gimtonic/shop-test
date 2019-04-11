929e9fdf81e2665e4bdb7ba1d8a9c1e482a2ac5a
Антон Королев <a.korolev@ddemo.ru>
2019-04-11 20:47:09 +0700

    Добавил модели

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $fillable = ['name','slug'];
+
+    /**
+     * Связь многие ко многим с таблицей продукты
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function category()
+    {
+        return $this->belongsToMany(Product::class);
+    }
 }
--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable=['name'];
+
+    /**
+     * Связь многие ко многим с таблицей категории
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function category()
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }
